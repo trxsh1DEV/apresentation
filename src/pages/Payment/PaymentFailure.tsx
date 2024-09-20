aa5bef5291c7404310aa9e32a56d98ae505b2803
trxsh1DEV <yagosousa630@gmail.com>
2024-09-20 11:44:55 -0300

    adjust payment failure

--- src/pages/Payment/PaymentFailure.tsx
+++ src/pages/Payment/PaymentFailure.tsx
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 
 const PaymentFailurePage: FC = () => {
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-xl">
-      <h1 className="text-2xl font-bold mb-4">Payment Failed</h1>
+    <div className="max-w-md mx-auto mt-10 p-6 rounded-lg shadow-xl">
+      <h1 className="text-2xl font-bold mb-4 text-black">Falha no pagamento</h1>
       <p className="text-red-500 mb-4">
-        We're sorry, but your payment could not be processed. Please try again
-        or contact support if the problem persists.
+        Lamentamos, mas não foi possível processar o seu pagamento. Por favor,
+        tente novamente ou entre em contato com o suporte se o problema
+        persistir.
       </p>
       <Link
         to="/payment"
         className="block w-full text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       >
-        Try Again
+        Tente novamente
       </Link>
     </div>
   );
