aa96be9f95252ef7f2c2d7cd69cb6e591d17ecc4
trxsh1DEV <yagosousa630@gmail.com>
2024-10-14 01:14:41 -0300

    Improvements Custom Page, now working as grid separate by Cards

--- src/pages/Custom/Custom.tsx
+++ src/pages/Custom/Custom.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,96 +10,220 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import requestWithToken from "@/utils/request";
 import { useToast } from "@/components/ui/use-toast";
+import {
+  Command,
+  CommandInput,
+  CommandList,
+  CommandItem,
+  CommandEmpty,
+  CommandGroup,
+} from "@/components/ui/command";
+import { useDebounce } from "@uidotdev/usehooks";
+
+interface App {
+  name: string;
+  appId: string;
+  version: string;
+}
+
+const ProhibitedSoftwareInput: FC<{ onSelect: (name: string) => void }> = ({
+  onSelect,
+}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [apps, setApps] = useState<App[]>([]);
+  const debouncedSearchTerm = useDebounce(searchTerm, 800);
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      const fetchApps = async () => {
+        const response = await requestWithToken.get(
+          `/software/repository/${debouncedSearchTerm}`
+        );
+        setApps(response.data);
+      };
+
+      fetchApps();
+    } else {
+      setApps([]);
+    }
+  }, [debouncedSearchTerm]);
+
+  return (
+    <div className="mb-4">
+      <Command>
+        <CommandInput
+          placeholder="Softwares Proibidos..."
+          value={searchTerm}
+          onValueChange={setSearchTerm}
+        />
+        <CommandList>
+          {apps.length > 0 && (
+            <>
+              <CommandEmpty>Nenhum resultado encontrado.</CommandEmpty>
+              <CommandGroup
+                heading="Resultados da Pesquisa"
+                className="bg-background absolute"
+              >
+                {apps.map((app) => (
+                  <CommandItem
+                    key={app.name}
+                    onSelect={() => {
+                      onSelect(app.name);
+                      setSearchTerm(app.name);
+                    }}
+                  >
+                    {app.name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </>
+          )}
+        </CommandList>
+      </Command>
+    </div>
+  );
+};
 
 const AddItemPage: React.FC = () => {
-  const [inputValue, setInputValue] = useState<string>("");
+  const [customInputValue, setCustomInputValue] = useState<string>("");
   const [selectedField, setSelectedField] = useState<string>("");
+  const [prohibitedSoftware, setProhibitedSoftware] = useState<string>("");
   const { toast } = useToast();
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
+  const handleCustomInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCustomInputValue(event.target.value);
   };
 
   const handleSelectChange = (value: string) => {
     setSelectedField(value);
   };
 
-  const handleAddClick = async () => {
-    if (!inputValue || !selectedField) return;
+  const handleProhibitedSoftwareSelect = (name: string) => {
+    setProhibitedSoftware(name);
+  };
+
+  const handleAddCustom = async () => {
+    if (!customInputValue || !selectedField) return;
 
     try {
       await requestWithToken.post("/company/custom", {
         customType: selectedField,
-        value: [inputValue],
+        value: [customInputValue],
       });
-      // console.log("Item adicionado com sucesso:", response.data);
-      setInputValue(""); // Limpa o input após o envio
-      setSelectedField(""); // Limpa o select após o envio
+      setCustomInputValue("");
+      setSelectedField("");
       toast({
         title: "Sucesso",
         className: "bg-success border-zinc-100",
         variant: "destructive",
-        description: `Item '${inputValue}' adicionado com sucesso`,
+        description: `Item '${customInputValue}' adicionado com sucesso`,
       });
     } catch (error: any) {
       toast({
         title: "Erro",
-        // className: "bg-success border-zinc-100",
         variant: "destructive",
         description: `Ocorreu um problema para adicionar esse item. Erro ${error.message}`,
       });
     }
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <div className="w-full max-w-md rounded-lg shadow-lg p-6">
-        <h1 className="text-2xl font-bold text-center mb-4">
-          Adicionar Customização
-        </h1>
-        <Input
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder="Digite o valor que quer adicionar"
-          className="mb-4 text-lg"
-        />
-        <div className="">
-          {/* <Label
-            htmlFor="department_ref"
-            className="text-2xl font-medium text-gray-700 dark:text-gray-300 "
-          >
-            Departamento
-          </Label> */}
+  const handleAddProhibitedSoftware = () => {
+    if (!prohibitedSoftware) return;
+    // Lógica para adicionar o software proibido
+    console.log("Software Proibido adicionado:", prohibitedSoftware);
+    setProhibitedSoftware("");
+    toast({
+      title: "Sucesso",
+      className: "bg-success border-zinc-100",
+      variant: "destructive",
+      description: `Software '${prohibitedSoftware}' adicionado à lista de proibidos`,
+    });
+  };
 
-          <Select onValueChange={handleSelectChange}>
-            <SelectTrigger
-              id="choice"
-              className="w-full dark:bg-slate-800 px-3 py-2 text-lg ring-ring focus:outline-none focus:ring-4 focus:ring-ring focus:ring-offset-2"
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold text-center mb-6">
+        Adicionar Customizações
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* Card para Customização Geral */}
+        <Card className="flex flex-col text-center">
+          <CardHeader>
+            <CardTitle>Customização Geral</CardTitle>
+          </CardHeader>
+          <CardContent className="flex-grow">
+            <Select onValueChange={handleSelectChange}>
+              <SelectTrigger className="mb-4">
+                <SelectValue placeholder="Selecione uma das opções" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Escolha sua customização</SelectLabel>
+                  <SelectItem value="local">Local</SelectItem>
+                  <SelectItem value="department">Departamento</SelectItem>
+                  <SelectItem value="collaborator">Colaborador</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Input
+              value={customInputValue}
+              onChange={handleCustomInputChange}
+              placeholder="Digite o valor que quer adicionar"
+              className="mb-4"
+            />
+          </CardContent>
+          <CardFooter>
+            <Button onClick={handleAddCustom} className="w-full text-lg">
+              Adicionar Customização
+            </Button>
+          </CardFooter>
+        </Card>
+
+        {/* Card para Softwares Proibidos */}
+        <Card className="flex flex-col text-center">
+          <CardHeader>
+            <CardTitle>Softwares Proibidos</CardTitle>
+          </CardHeader>
+          <CardContent className="flex-grow">
+            <ProhibitedSoftwareInput
+              onSelect={handleProhibitedSoftwareSelect}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button
+              onClick={handleAddProhibitedSoftware}
+              className="w-full text-lg"
             >
-              <SelectValue
-                id="department"
-                data-select-type="edit-field"
-                placeholder="Selecione uma das opções"
-              />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Escolha sua customização</SelectLabel>
-                <SelectItem value="local">Local</SelectItem>
-                <SelectItem value="department">Departamento</SelectItem>
-                <SelectItem value="collaborator">Colaborador</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <Button
-          onClick={handleAddClick}
-          className="w-full  text-white hover:bg-indigo-500 mt-10 text-xl"
-        >
-          Adicionar
-        </Button>
+              Adicionar Software
+            </Button>
+          </CardFooter>
+        </Card>
+
+        {/* Card para Futuras Customizações */}
+        <Card className="flex flex-col text-center">
+          <CardHeader>
+            <CardTitle>Futuras Customizações</CardTitle>
+          </CardHeader>
+          <CardContent className="flex-grow">
+            <p>Espaço reservado para futuras opções de customização.</p>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full text-lg" disabled>
+              Em breve
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
